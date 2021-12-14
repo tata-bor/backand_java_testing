ce26ebd0d5d5457612364ad94b818367ab9b86f6
tata-bor <tatiana.bormatova@gmail.com>
2021-12-14 22:44:08 +0300

    Homework. Lesson 3. Rest Assured Тестирование внешнего сервиса при помощи Rest Assured

---
+++ src/test/java/com/geekbrains/backend/test/imgur/ImgurApiFunctionalTest.java
@@ -0,0 +1,173 @@
+package com.geekbrains.backend.test.imgur;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ImgurApiFunctionalTest extends ImgurApiAbstractTest {
+
+    private static String CURRENT_IMAGE_ID;
+
+    @Test
+    @Order(1)
+    void getAccountBase() {
+        String userName = "tatabor";
+        given()
+                .spec(requestSpecification)
+                .log()
+                .all()
+                .expect()
+                .body("data.id", is(157947641))
+                .log()
+                .all()
+                .when()
+                .get("account/" + userName);
+    }
+
+    @Test
+    @Order(2)
+    void postImageTest() {
+        CURRENT_IMAGE_ID = given()
+                .spec(requestSpecification)
+                .multiPart("image", getFileResource("img.jpg"))
+                .formParam("name", "Picture")
+                .formParam("title", "The best picture!")
+                .log()
+                .all()
+                .expect()
+                .body("data.size", is(46314))
+                .body("data.type", is("image/jpeg"))
+                .body("data.name", is("Picture"))
+                .body("data.title", is("The best picture!"))
+                .log()
+                .all()
+                .when()
+                .post("upload")
+                .body()
+                .jsonPath()
+                .getString("data.id");
+    }
+
+    @Test
+    @Order(3)
+    void getImageTest() {
+        given()
+                .spec(requestSpecification)
+                .log()
+                .all()
+                .expect()
+                .body("data.type", is("image/jpeg"))
+                .body("data.name", is("Picture"))
+                .body("data.title", is("The best picture!"))
+                .log()
+                .all()
+                .when()
+                .get("image/" + CURRENT_IMAGE_ID);
+    }
+
+    @Test
+    @Order(4)
+    void updateImageInformationTest() {
+        given()
+                .spec(requestSpecification)
+                .formParam("title", "Mem")
+                .formParam("description", "Best mem mem mem mem!")
+                .log()
+                .all()
+                .expect()
+                .body("data", is(true))
+                .body("success", is(true))
+                .body("status", is(200))
+                .log()
+                .all()
+                .when()
+                .post("image/" + CURRENT_IMAGE_ID);
+    }
+
+    @Test
+    @Order(5)
+    void getImageTestAfterUpdate() {
+        given()
+                .spec(requestSpecification)
+                .log()
+                .all()
+                .expect()
+                .body("data.description", is("Best mem mem mem mem!"))
+                .body("data.title", is("Mem"))
+                .log()
+                .all()
+                .when()
+                .get("image/" + CURRENT_IMAGE_ID);
+    }
+
+    @Test
+    @Order(6)
+    void deleteImageById() {
+        given()
+                .spec(requestSpecification)
+                .log()
+                .all()
+                .expect()
+                .body("status", is(200))
+                .log()
+                .all()
+                .when()
+                .delete("image/" + CURRENT_IMAGE_ID);
+    }
+
+    private static String COMMENT_ID;
+
+    @Test
+    @Order(7)
+    void testCreateComment() {
+        COMMENT_ID = given()
+                .spec(requestSpecification)
+                .formParam("image_id", "8xGCvWR")
+                .formParam("comment", "Hello world")
+                .log()
+                .all()
+                .expect()
+                .body("success", is(true))
+                .body("status", is(200))
+                .log()
+                .all()
+                .when()
+                .post("comment")
+                .body()
+                .jsonPath()
+                .getString("data.id");
+    }
+
+    @Test
+    @Order(8)
+    void getCommentTest() {
+        given()
+                .spec(requestSpecification)
+                .log()
+                .all()
+                .expect()
+                .log()
+                .all()
+                .when()
+                .get("comment/" + COMMENT_ID);
+    }
+    @Test
+    @Order(9)
+    void deleteCommentById() {
+        given()
+                .spec(requestSpecification)
+                .log()
+                .all()
+                .expect()
+                .body("status", is(200))
+                .log()
+                .all()
+                .when()
+                .delete("comment/" + COMMENT_ID);
+    }
+}
